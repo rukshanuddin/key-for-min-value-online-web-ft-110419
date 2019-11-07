c4a944f1e92aa274e1ce3a9bc178b912f2152e64
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 19:22:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name = 100
+  name_hash.each_value { |val| name = val if val < name }
+  name_hash[100]
 end
+
+
+psuedocode
+
