665d324be7cffaf98a94c11b90e9ae91b5bf9aa4
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 19:50:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,11 @@
 
 
 def key_for_min_value(name_hash)
-  name = name_hash[0]
-  name_hash.each_value { |val| name = val if val < name }
+  
+  name_hash.each_value do |val|
+     name = val if name.exists?
+     name = val if val < name 
+   end
   name_hash.each { |n, val| return n if val == name }
   nil
 end
